026b24086d787791cdea7689c7fcd50ea5260c20
Milan Panchal <59117562+panchalmilan@users.noreply.github.com>
2020-05-09 10:13:15 +0530

    added new features

--- public/js/app.js
+++ public/js/app.js
@@ -2,28 +2,27 @@ const weatherForm = document.querySelector('form');
 const search = weatherForm.querySelector('input');
 const successMsg = document.querySelector('#msg1');
 const successMsg2 = document.querySelector('#msg2');
+const enterLoc = document.querySelector('#enteredLoc');
 
 weatherForm.addEventListener('submit', (e) => {
+  enterLoc.textContent = '';
   successMsg.textContent = 'Loading...';
   successMsg2.textContent = '';
   fetch('/weather?address=' + search.value).then(
     (response) => {
       response.json().then((dataObj) => {
         if (dataObj.error) {
+          enterLoc.textContent = '';
           successMsg.textContent = dataObj.error;
           successMsg2.textContent = '';
         } else {
+          // enterLoc.textContent = search.value.style.textTransform.uppercase;
+          enterLoc.textContent = search.value;
           successMsg2.textContent = dataObj.location;
           successMsg.textContent = dataObj.forecast;
         }
-        clearSearchBox();
       });
     }
   );
   e.preventDefault();
 });
-const clearSearchBox = () => {
-  setTimeout(() => {
-    search.value = '';
-  }, 7000);
-};
